ee4d1b4a37abf2139eff401c8ff78a5c9a987401
Rolso22 <ramagf@ynadex.ru>
2022-08-16 21:30:09 +0300

    feat: trying to change tree

--- vizualjit.py
+++ vizualjit.py
@@ -2,6 +2,7 @@ import ast
 import json
 import types
 
+import json_converter
 from json_converter import convert_to_json
 from parser import Parser
 
@@ -30,7 +31,7 @@ def func1(json_str, filter_value):
     print()
 
     json_ast = convert_to_json(code)
-    print(json.dumps(json_ast, indent=3))
+    # print(json.dumps(json_ast, indent=3))
     with open("test2.json", "w") as fp:
         fp.write(json.dumps(json_ast, indent=3))
 
@@ -68,9 +69,53 @@ def test_module(my):
 
 # ----------------------------------------------------------
 
-my = compile_ast("test.json")
+my = compile_ast("test.json")  # without count
 test_module(my)
 
 change_code()
-my = compile_ast("test2.json")
+my = compile_ast("test2.json")  # with count
 test_module(my)
+
+# -----------------------------------------------------------
+
+print()
+str3 = '''
+def f(x, y):
+    return x * y
+'''
+
+fun = ast.parse(str3)
+# print(json.dumps(json_converter.convert_to_json(fun), indent=3))
+
+fun_comp = compile(fun, '<string>', mode="exec")
+
+fun_module = types.ModuleType(
+    'fun',
+    'doc')
+exec(fun_comp, fun_module.__dict__)
+
+result = fun_module.f(3, 5)  # 3 * 5
+print(result)
+
+
+class ChangeFun(ast.NodeTransformer):
+
+    def visit_Return(self, node):
+        value = node.value
+        return ast.Return(value=ast.BinOp(left=value.left,
+                                          op=ast.Add(),
+                                          right=value.right))
+
+
+fun2 = ast.fix_missing_locations(ChangeFun().visit(fun))
+# print(json.dumps(json_converter.convert_to_json(fun2), indent=3))
+
+fun_comp = compile(fun, '<string>', mode="exec")
+
+fun_module = types.ModuleType(
+    'fun',
+    'doc')
+exec(fun_comp, fun_module.__dict__)
+
+result = fun_module.f(3, 5)  # 3 + 5
+print(result)
